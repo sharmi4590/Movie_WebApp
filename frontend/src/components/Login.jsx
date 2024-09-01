2e5a584e2fc26fb23a7bfee9fc2d9d2c5b43c9c3
sharmi4590 <bheemisettysharmila@gmail.com>
2024-09-01 13:18:16 +0530

    Start

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/v1/auth/login', { email, password });
+      const response = await axios.post('https://movie-webapp-whib.onrender.com/api/v1/auth/login', { email, password });
       localStorage.setItem('token', response.data.token);
       localStorage.setItem('user', JSON.stringify(response.data.user));
       console.log('Login successful:', response.data.user);
